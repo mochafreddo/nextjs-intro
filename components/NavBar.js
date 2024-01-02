bc8568700c97690cd207d38dafef913ccf4aa9fd
mochafreddo <corc411@gmail.com>
2024-01-03 01:57:06 +0900

    feat: update NavBar styling and remove CSS module
    This commit updates the styling of the NavBar component, removing the CSS module previously used and instead incorporating the styles directly within the component. The active link now changes color to yellow for better visibility. Additionally, the styling for the 'Hello' text in the Home page has been updated.

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,28 +1,30 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
-import styles from "./NavBar.module.css";
 
 export default function NavBar() {
   const router = useRouter();
   return (
     <nav>
-      <Link
-        className={`${styles.link} ${
-          router.pathname === "/" ? styles.active : ""
-        }`}
-        href="/"
-      >
+      <Link className={router.pathname === "/" ? "active" : ""} href="/">
         Home
       </Link>
       <Link
-        className={[
-          styles.link,
-          router.pathname === "/about" ? styles.active : "",
-        ].join(" ")}
+        className={router.pathname === "/about" ? "active" : ""}
         href="/about"
       >
         About
       </Link>
+      <style jsx>{`
+        nav {
+          background-color: tomato;
+        }
+        a {
+          text-decoration: none;
+        }
+        .active {
+          color: yellow;
+        }
+      `}</style>
     </nav>
   );
 }
