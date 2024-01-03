e0919f06b2b3972a62d1c6c81e9e4e4a8a0ed4a6
mochafreddo <corc411@gmail.com>
2024-01-03 22:47:39 +0900

    feat: update routing and link structure
    - Change the routing structure in `pages/index.js` to use title and id as parameters instead of query parameters.
    - Update the Link component to match the new routing structure.
    - Remove the unused Detail component.

--- pages/index.js
+++ pages/index.js
@@ -6,10 +6,7 @@ import { useEffect, useState } from "react";
 export default function Home({ results }) {
   const router = useRouter();
   const onClick = (id, title) => {
-    router.push(
-      { pathname: `/movies/${id}`, query: { title } },
-      `/movies/${id}`,
-    );
+    router.push(`/movies/${title}/${id}`);
   };
   return (
     <div className="container">
@@ -22,13 +19,7 @@ export default function Home({ results }) {
         >
           <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} />
           <h4>
-            <Link
-              href={{
-                pathname: `/movies/${movie.id}`,
-                query: { title: movie.original_title },
-              }}
-              as={`/movies/${movie.id}`}
-            >
+            <Link href={`/movies/${movie.original_title}/${movie.id}`}>
               {movie.original_title}
             </Link>
           </h4>
